834c53b6a015bc0d6b17f707a766b569d8c4fe6b
Freeassassin(LapTop) <farbodemzadeh@gmail.com>
2019-11-29 13:49:49 -0500

    auton

--- Auton3 degree/src/main.cpp
+++ Auton3 degree/src/main.cpp
@@ -47,6 +47,55 @@ void move(int distance,int speed, bool collect = false)
   RightDriveR.stop();
 
 }
+void moveX(int distance,int speed, bool collect = false)
+{ 
+  if(collect)
+  {
+    ScooperL.setVelocity(90, percentUnits::pct);
+    ScooperR.setVelocity(90, percentUnits::pct);
+    ScooperL.rotateFor(-(distance+1000), rotationUnits::deg, false);
+    ScooperR.rotateFor(-(distance+1000), rotationUnits::deg, false);
+  }
+  LeftDriveF.setVelocity(speed, percentUnits::pct);
+  RightDriveF.setVelocity(speed, percentUnits::pct);
+  LeftDriveR.setVelocity(speed, percentUnits::pct);
+  RightDriveR.setVelocity(speed, percentUnits::pct);
+  LeftDriveF.rotateFor(distance, rotationUnits::deg, false);
+  RightDriveF.rotateFor(distance, rotationUnits::deg, false);
+  LeftDriveR.rotateFor(distance, rotationUnits::deg, false);
+  RightDriveR.rotateFor(distance, rotationUnits::deg, true);
+  LeftDriveF.stop();
+  RightDriveF.stop();
+  LeftDriveR.stop();
+  RightDriveR.stop();
+
+}
+void moveT(int seconds,int speed, bool collect = false)
+{ 
+  if(collect)
+  {
+    ScooperL.setVelocity(80, percentUnits::pct);
+    ScooperR.setVelocity(80, percentUnits::pct);
+    ScooperL.spin(directionType::rev);
+    ScooperR.spin(directionType::rev);  
+  }
+  LeftDriveF.setVelocity(speed, percentUnits::pct);
+  RightDriveF.setVelocity(speed, percentUnits::pct);
+  LeftDriveR.setVelocity(speed, percentUnits::pct);
+  RightDriveR.setVelocity(speed, percentUnits::pct);
+  LeftDriveF.spin(directionType::fwd);  
+  RightDriveF.spin(directionType::fwd);
+  LeftDriveR.spin(directionType::fwd);
+  RightDriveR.spin(directionType::fwd);
+  vexDelay(seconds);
+  LeftDriveF.stop();
+  RightDriveF.stop();
+  LeftDriveR.stop();
+  RightDriveR.stop();
+  vexDelay(250);
+  ScooperL.stop();
+  ScooperR.stop();  
+}
 //The fucntion to orient the robot based on seconds and speed
 void turn(int distance,int speed)
 {
@@ -73,14 +122,14 @@ void strafe(int distance,int speed)
 }
 void strafeX(int distance,int speed)
 {
-  LeftDriveF.setVelocity(speed-12, percentUnits::pct);
-  RightDriveF.setVelocity(speed-12, percentUnits::pct);
-  LeftDriveR.setVelocity(speed, percentUnits::pct);
-  RightDriveR.setVelocity(speed, percentUnits::pct);
+  LeftDriveF.setVelocity(speed, percentUnits::pct);
+  RightDriveF.setVelocity(speed, percentUnits::pct);
+  LeftDriveR.setVelocity(speed-20,percentUnits::pct);
+  RightDriveR.setVelocity(speed-20, percentUnits::pct);
   LeftDriveF.rotateFor(distance, rotationUnits::deg, false);
   RightDriveF.rotateFor(-distance, rotationUnits::deg, false);
-  LeftDriveR.rotateFor(-distance, rotationUnits::deg, false);
-  RightDriveR.rotateFor(distance, rotationUnits::deg, true);  
+  LeftDriveR.rotateFor(-distance+200, rotationUnits::deg, false);
+  RightDriveR.rotateFor(distance-200, rotationUnits::deg, true);  
 }
 //The function to fully expand the robot
 void expand()
@@ -89,7 +138,7 @@ void expand()
   ArmR.setVelocity(90, percentUnits::pct);
   ArmR.spin(directionType::rev);  
   ArmL.spin(directionType::rev);
-  vexDelay(1600);
+  vexDelay(1400);
   ArmL.stop();
   ArmR.stop();
   ArmL.setVelocity(100, percentUnits::pct);
@@ -116,33 +165,76 @@ void stack()
   double i = 0;
   while(i <18)
   {
-    
-    ArmL.setVelocity(-0.2*(pow(i,2))+70, percentUnits::pct);
-    ArmR.setVelocity(-0.2*(pow(i,2))+70, percentUnits::pct);
+    LeftDriveF.setVelocity(-0.08*(pow(i,2))+20, percentUnits::pct);
+    RightDriveF.setVelocity(-0.08*(pow(i,2))+20, percentUnits::pct);
+    LeftDriveR.setVelocity(-0.08*(pow(i,2))+20, percentUnits::pct);
+    RightDriveR.setVelocity(-0.08*(pow(i,2))+20, percentUnits::pct);
+    ArmL.setVelocity(-0.2*(pow(i,2))+65, percentUnits::pct);
+    ArmR.setVelocity(-0.2*(pow(i,2))+65, percentUnits::pct);
+    LeftDriveF.spin(directionType::fwd);  
+    RightDriveF.spin(directionType::fwd);
+    LeftDriveR.spin(directionType::fwd);
+    RightDriveR.spin(directionType::fwd);
     ArmL.spin(directionType::rev);
     ArmR.spin(directionType::rev);
-    vexDelay(150);
+    vexDelay(145);
     i += 1;    
   }
   ArmL.stop();
   ArmR.stop();
+  LeftDriveF.stop();  
+  RightDriveF.stop();
+  LeftDriveR.stop();
+  RightDriveR.stop();
 }
 int main() { 
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   // Expand the Robot
   //expand(); //2800
-/*  */
+/*  
   move(1300, 40, true);// +250
   move(-1100, 40,true);// +250  
   strafe(-870, 40);
   //turn(-120,40);
   move(800, 40, true);// +250
-  move(-700, 40);
+  move(-600, 40);
   strafeX(1300, 70);
   turn(1000, 40);
-  move(700, 40);
+  moveT(900, 40);
+  ScooperL.setVelocity(70, percentUnits::pct);
+  ScooperR.setVelocity(70, percentUnits::pct);  
+  ScooperR.spin(directionType::fwd);  
+  ScooperL.spin(directionType::fwd);
+  vexDelay(200);
+  ScooperL.stop();
+  ScooperR.stop();
   stack(); //2880
   move(-500, 100); 
 //degree baed
+*/
+/*  
+  //expand(); //2800
+  move(1400, 60, true);// +250
+  move(-1075, 90,true);// +250 
+  strafe(-850, 85); 
+  turn(-75,70);
+  moveX(800, 60, true);// +250
+  //move(-550, 90);
+  //vexDelay(150);
+  turn(880, 80);
+  //strafeX(1300, 90);
+  //turn(850, 80);
+  moveT(1400, 80);
+  ScooperL.setVelocity(90, percentUnits::pct);
+  ScooperR.setVelocity(90, percentUnits::pct);  
+  ScooperR.spin(directionType::fwd);  
+  ScooperL.spin(directionType::fwd);
+  vexDelay(300);
+  ScooperL.stop();
+  ScooperR.stop();  
+  stack(); //2880
+  move(-500, 100);  
+*/
+
 }
